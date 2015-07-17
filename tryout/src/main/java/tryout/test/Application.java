261d714a22538c953a4689295d7dad20da1626de
pmucha <peter.mucha@affinitas.de>
2015-07-17 17:20:42 +0200

    fixed sample

--- tryout/src/main/java/tryout/test/Application.java
+++ tryout/src/main/java/tryout/test/Application.java
@@ -7,8 +7,8 @@ public class Application {
 	public static void main(String[] args) {
 		
 		
-		tryout.test2.Container c2 = new Container() {
-		};
+		tryout.test2.Container c2 = new Container() {};
+		c2.start();
 		
 		tryout.test.Container container = new tryout.test.Container(){
 
